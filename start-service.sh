e5a207977c11a4f5fc94a6122c37ee0d6f2e0ed5
Mohankumar Ramachandran <>
2022-01-16 18:55:00 +0530

    backup

--- start-service.sh
+++ start-service.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 kill -9 $(cat /process.pid 2>/dev/null) 2>/dev/null && sleep 5
-/usr/bin/code-server --config /config.yaml &> /application.log &
+/usr/bin/code-server --config /config.yaml &>> /application.log &
 echo $! > /process.pid
